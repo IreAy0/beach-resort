4ef011330a41a9977a59b3f22c9583aae90b977a
Confidence <confidenceigwe@gmail.com>
2019-08-29 13:29:45 +0100

    update

--- src/Context.js
+++ src/Context.js
@@ -3,48 +3,41 @@ import items from "./data"
 const RoomContext = React.createContext();
 
  class RoomProvider extends Component {
-state={
-    rooms:[],
-    sortedRooms:[],
-    featuredRooms:[],
-    loading:true
-};
-componentDidMount(){
-    let rooms = this.formatData(items);
-   
-  let featuredRooms = rooms.filter(room => room.featured === true);
-  this.setState({
-      rooms:rooms,featuredRooms:featuredRooms,sortedRooms:rooms,loading:false
-  })
-}
 
-formatData(items){
-let tempItems = items.map(item =>  {
-let id= item.sys.id;
-let images = item.fields.images.map(image=> 
-    image.fields.file.url);
-let room = {...item.fields,images,id}
-return room;
+    state={
+        rooms:[],
+        sortedRooms:[],
+        featuredRooms:[],
+        loading:true
+    };
 
+    componentDidMount(){
+        let rooms = this.formatData(items);
+    
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+        rooms:rooms,featuredRooms:featuredRooms,sortedRooms:rooms,loading:false
+    })
+    }
 
-});
-return tempItems
-}
-
-getRoom = slug => {
-    let tempRooms ={...this.state.rooms};
-    const room = tempRooms.find(room => room.slug === slug);
+    formatData(items){
+        let tempItems = items.map(item =>  {
+        let id= item.sys.id;
+        let images = item.fields.images.map(image=> 
+            image.fields.file.url);
+        let room = {...item.fields,images,id}
     return room;
-};
+    });
+    return tempItems
+    }
 
-
-
-
-
-
-
-
-    render() {
+    getRoom = slug => {
+        let tempRooms =[...this.state.rooms];
+        const room = tempRooms.find(room => room.slug === slug);
+        return room;
+    };
+    
+  render() {
         return (
            <RoomContext.Provider value={{...this.state, getRoom: this.getRoom}}>
                 {this.props.children}
